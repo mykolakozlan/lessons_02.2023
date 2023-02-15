ce7afef898ed581b18dd4c93a7982a3980099428
NikolasK88 <maxsuccess4@gmail.com>
2023-02-15 21:13:57 +0200

    created lesson_2.py
    data types

---
+++ lesson_2.py
@@ -0,0 +1,96 @@
+# Numbers
+
+# int - 9
+# float - 8.99899909991
+
+# decimal - 8.998999099997 більш точні!
+# complex - (3j)
+
+# String
+# str - строки
+
+# Boolean
+# bool(True/False)
+
+# list []
+# dict {"key": 77}
+# tuple ()
+# set {}
+# range
+
+# None
+
+
+PI = 3.14
+YEAR = 12
+DAY = 24
+
+
+# value_int = 11
+#
+# value_like_int = value_int
+#
+# value_int = "Hello World"
+#
+# print(value_like_int)   - змінні незмінні типи даних
+
+
+value_int_1 = 9
+value_int_2 = 2
+
+
+# type() - визначити тип даних
+
+# print(value_int_1, type(value_int_1))
+
+result_add = value_int_1 + value_int_2
+result_mult = value_int_1 * value_int_2
+result_sub = value_int_1 - value_int_2
+result_div = value_int_1 / value_int_2 # ділення
+result_div_1 = value_int_1 // value_int_2 # цілочисслене ділення
+result_div_2 = value_int_1 % value_int_2 # залишок після ділення
+result_power = value_int_1 ** value_int_2 #ступінь
+result_root = value_int_1 ** 0.5 #корінь
+
+# print(result_sub)
+# print(result_root, type(result_root))
+
+value_float_1 = 1
+value_float_2 = 2.0
+
+
+# print(id(value_float_1))
+
+######################################
+
+# Strings
+
+value_str1 = "Football club \"Liverpool\". \nGame\twill\tbe\ttoday "
+value_str2 = 'Football club "Liverpool"'
+value_str3 = """Hello"""
+value_str4 = '''Hello'''
+
+value_str5 = r"Football club \"Liverpool\". \nGame\twill\tbe\ttoday " #raw string сира строка
+
+
+filename = 'document'
+extention = 'doc'
+path = "Documents"
+
+value_str6 = f"{path}/{filename}.{extention}"  # f - string дозволяє працювати з змінними в строчці +
+
+# value = "Hello" + " World"
+# value = "Hello" * 2
+
+number = 2
+
+
+value = f"Hello {number}"
+
+print(value)
+
+
+
+
+
+
